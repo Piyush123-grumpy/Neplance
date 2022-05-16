5684827276630a6ccaad3fa27b57c2fa6dd34d72
kabir <k4birxakya@gmail.com>
2022-05-16 15:10:34 +0545

    Login backend done

--- account/urls.py
+++ account/urls.py
@@ -6,5 +6,6 @@ urlpatterns = [
     path('', views.home,name='home'),
     path('register/', views.register,name='register'),
     path('signup_freelancer/',views.signup_freelancer,name='signup_freelancer'),
-    path('signup_employer/',views.signup_employer,name='signup_employer')
+    path('signup_employer/',views.signup_employer,name='signup_employer'),
+    path('login/', views.loginpage,name='login_dashboard'),
 ]
--- account/views.py
+++ account/views.py
@@ -1,7 +1,9 @@
 
 from django.http import HttpResponse
+from django.contrib.auth import authenticate, login
 from django.shortcuts import render,redirect 
 from .forms import FreelancerSignUpForm,UserCreationForm,EmployerSignUpForm
+from django.contrib import auth, messages
 
 # Create your views here.
 def home(request):
@@ -30,3 +32,33 @@ def signup_employer(request):
             return redirect('/')
     context={"form":form}
     return render (request,"account/employer_signup.html",context)
+
+def loginpage(request):
+    if request.user.is_authenticated:
+        messages.warning(request,"You are already logged in")
+        return redirect('/')
+
+    else:
+        if request.method == 'POST':
+            name = request.POST.get('username')
+            print(name)
+            passwd = request.POST.get('password')
+            print(passwd)
+            user = authenticate(request, username=name, password=passwd)
+            
+            if user is not None and user.is_freelancer==True:
+                login(request, user)
+                print("sss")
+                messages.success(request,"Logged in Successfully")
+                return redirect("dashboard/")
+            elif user is not None and user.is_employer==True:
+                login(request, user)
+                print("ass")
+                messages.success(request,"Logged in Successfully")
+                return redirect("dashboard/")
+            
+            else:
+                messages.error(request, "Invalid Username or Password")
+                print("Okay")
+                return redirect("/login")
+        return render(request,"account/login.html")
