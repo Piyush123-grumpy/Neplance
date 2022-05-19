8dbe75d6fa79437a222d0cc96aa9ac39e293f414
kabir <k4birxakya@gmail.com>
2022-05-19 08:35:30 +0545

    login ui

--- account/urls.py
+++ account/urls.py
@@ -6,5 +6,5 @@ urlpatterns = [
     path('register/', views.register,name='register'),
     path('signup_freelancer/',views.signup_freelancer,name='signup_freelancer'),
     path('signup_employer/',views.signup_employer,name='signup_employer'),
-    path('login/', views.loginpage,name='login_dashboard'),
+    path('login/', views.loginpage,name='login'),
 ]
