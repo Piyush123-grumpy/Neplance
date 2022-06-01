c7fccd46c0e9747f56a3e65e9eceafe1f64cc6c6
Pooja9009 <miss.poojarijal@gmail.com>
2022-06-01 19:57:29 +0545

    homepage

--- Neplance/settings.py
+++ Neplance/settings.py
@@ -85,7 +85,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'Neplance',
         'HOST': '127.0.0.2',
-        'PORT': '3307',
+        'PORT': '3306',
         'USER': 'root',
         'PASSWORD': '',
     }
