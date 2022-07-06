76cd1c187467cba400629369956888945a9d1a11
abhinna1 <manandharabhinna@gmail.com>
2022-07-06 14:13:30 +0545

    application testing completed.

--- gig/tests.py
+++ gig/tests.py
@@ -1,9 +1,11 @@
+from distutils.core import setup
+import json
 from django.test import SimpleTestCase, TestCase
 from django.urls import reverse, resolve
 from .views import addgigs
 from .models import Category, Gig
-from account.models import User
-
+from account.models import User, Employer, Freelancer
+import requests
 # Create your tests here.
 
 # URL Testing for add gig.
@@ -14,8 +16,11 @@ from account.models import User
 #         self.assertEqual(resolve(url).func,addgigs)
 
 class JobDetailUnitTestCase(TestCase):
+
     def createUser(self):
         user = User.objects.create(username='testuser', password='testpass', email='testuser123@gmail.com')
+        Freelancer.objects.create(user=user)
+        Employer.objects.create(user=user)
         return user
 
     def createCategory(self):
@@ -23,7 +28,7 @@ class JobDetailUnitTestCase(TestCase):
         category.category_name = "testCategory" # create category.
         category.save()
         return category
-    def createGig(self):
+    def createGig(self, user):
         gig = Gig()
         gig.title = 'test_gig'
         gig.description = 'desc'
@@ -31,12 +36,26 @@ class JobDetailUnitTestCase(TestCase):
         gig.city = 'Kathmandu'
         gig.area = 'Hattigauda'
         gig.pay = 100
-        gig.user = self.createUser()
+        gig.user = user
         gig.category = self.createCategory()
         gig.save()
         return gig
 
     def test_jobdetail_template(self):
-        gig = self.createGig()
-        response = self.client.get('/gig/jobdetail/1')
+        self.user = self.createUser()
+        self.gig = self.createGig(self.user)
+        response = self.client.get('/gig/jobdetail/'+str(self.gig.id))
         self.assertTemplateUsed(response, 'jobdetail.html')
+    
+    def test_apply_job(self):
+        self.user = self.createUser()
+        self.gig = self.createGig(self.user)
+        response = self.client.post('/gig/applyjson/', {'user':self.user.id, 'gig': self.gig.id})
+        self.assertEqual(response.content, b'6969')
+    
+    def test_double_apply_job(self):
+        self.user = self.createUser()
+        self.gig = self.createGig(self.user)
+        response1 = self.client.post('/gig/applyjson/', {'user':self.user.id, 'gig': self.gig.id})
+        response2 = self.client.post('/gig/applyjson/', {'user':self.user.id, 'gig': self.gig.id})
+        self.assertEqual(response2.content, b'69420')
