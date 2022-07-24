e8c6bb48134ba4b4e9a95b0cfa2ec5f8917fda3b
abhinna1 <manandharabhinna@gmail.com>
2022-07-24 18:15:30 +0545

    search countries fixed

--- gig/forms.py
+++ gig/forms.py
@@ -4,8 +4,10 @@ from .models import Gig
 class AddGigForm (forms.ModelForm):
     class Meta:
         model = Gig
-        fields = ['title', 'description', 'pay', 'category','image']
+        fields = ['title', 'company', 'image', 'description', 'country', 'pay', 'contact', 'category']
         # Custom attributes to ModelForm fields.
         widgets= {
             'description':forms.Textarea(attrs={'class': 'x', 'placeholder': 'Job Description'}),
+            'country':forms.Select(attrs={'class': 'country-input', 'placeholder': 'Job Description'}),
+            'category':forms.Select(attrs={'class': 'country-input', 'placeholder': 'Job Description'}),
         }
