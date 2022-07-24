2658f1a2a696b3e7116fd2b32f118fa4da2dcc27
abhinna1 <manandharabhinna@gmail.com>
2022-07-24 19:50:34 +0545

    add gig completed.

--- gig/forms.py
+++ gig/forms.py
@@ -4,7 +4,7 @@ from .models import Gig
 class AddGigForm (forms.ModelForm):
     class Meta:
         model = Gig
-        fields = ['title', 'company', 'image', 'description', 'country', 'pay', 'contact', 'category']
+        fields = ['title', 'company', 'image', 'description', 'country', 'city', 'area', 'pay', 'contact', 'category']
         # Custom attributes to ModelForm fields.
         widgets= {
             'description':forms.Textarea(attrs={'class': 'x', 'placeholder': 'Job Description'}),
